746a7eafa5527305bd53cc729ebfbb6814638639
Thomas Monvoisin <thomasmonvoisin@gmail.com>
2020-11-12 16:44:27 +0100

    Start Frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'open-uri'
+# require 'json'
+
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
+
+# puts "#{ingredients['drinks']} - #{user['bio']}"
+
+
+Dose.create(description: "5 cl")
+Dose.create(description: "1l")
+Dose.create(description: "3 cups")
